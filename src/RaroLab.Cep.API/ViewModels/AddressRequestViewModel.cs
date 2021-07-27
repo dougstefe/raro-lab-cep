7034b8f32ee3a8081bf96b23fa3d0695a791cb85
Douglas Lessa <dougstefe@gmail.com>
2021-07-27 10:06:34 -0300

    chore: implementar swagger

---
+++ src/RaroLab.Cep.API/ViewModels/AddressRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace RaroLab.Cep.API.ViewModels
+{
+    public class AddressRequestViewModel
+    {
+        /// <summary>
+        /// Zip code to be consulted. Use only numbers.
+        /// </summary>
+        [Required(ErrorMessage = "Field 'ZipCode' is required.")]
+        [RegularExpression("^[0-9]{8}$", ErrorMessage = "Field 'ZipCode' is invalid. Format = 99999999.")]
+        //[StringLength(8, ErrorMessage = "Field 'ZipCode' is invalid. StringLength = 8.")]
+        public string ZipCode { get; set; }
+    }
+}
