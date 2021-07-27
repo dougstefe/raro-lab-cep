a4d27f530758c427916d1f990ce82d9dcedf80ed
Douglas Lessa <dougstefe@gmail.com>
2021-07-27 10:07:10 -0300

    test: implementar test unitário da service

--- test/RaroLab.Cep.Unit.Tests/Mocks/Services/ViaCepAddressResponseModelMock.cs
+++ test/RaroLab.Cep.Unit.Tests/Mocks/Services/ViaCepAddressResponseModelMock.cs
@@ -1,7 +1,24 @@
-﻿namespace RaroLab.Cep.Unit.Tests.Mocks.Services
+﻿using Bogus;
+using RaroLab.Cep.Domain.Models.Services.ViaCep;
+
+namespace RaroLab.Cep.Unit.Tests.Mocks.Services
 {
     public static class ViaCepAddressResponseModelMock
     {
-
+        public static Faker<ViaCepAddressResponseModel> ViaCepAddressResponseModelFaker =>
+            new Faker<ViaCepAddressResponseModel>()
+            .CustomInstantiator(x => new ViaCepAddressResponseModel
+            {
+                Cep = x.Address.ZipCode("#####-###"),
+                Logradouro = x.Address.StreetAddress(false),
+                Complemento = x.Random.Words(3),
+                Bairro = x.Address.City(),
+                Localidade = x.Address.State(),
+                Uf = x.Address.StateAbbr(),
+                Ibge = x.Random.Number(232323, 999999).ToString(),
+                Gia = x.Random.Number(1000, 9999).ToString(),
+                Ddd = x.Random.Number(10, 99).ToString(),
+                Siafi = x.Random.Number(1000, 9999).ToString()
+            });
     }
 }
--- test/RaroLab.Cep.Unit.Tests/Services/CustomerServiceTest.cs
+++ test/RaroLab.Cep.Unit.Tests/Services/CustomerServiceTest.cs
@@ -1,8 +1,10 @@
-﻿using Moq;
+﻿using AutoMapper;
+using Moq;
 using RaroLab.Cep.API.Services;
 using RaroLab.Cep.API.ViewModels;
 using RaroLab.Cep.Domain.Interfaces;
 using RaroLab.Cep.Unit.Tests.Mocks.Customer;
+using RaroLab.Cep.Unit.Tests.Mocks.Services;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -11,19 +13,30 @@ namespace RaroLab.Cep.Unit.Tests.Services
     public class CustomerServiceTest
     {
         private readonly Mock<IViaCepService> _viaCepMock;
+        private readonly Mock<IMapper> _mapperMock;
 
         public CustomerServiceTest()
         {
-            _viaCepMock = new Mock<IViaCepService>(); ;
+            _viaCepMock = new Mock<IViaCepService>();
+            _mapperMock = new Mock<IMapper>();
         }
 
         [Fact]
         public async Task GetAddressByZipCodeAsync_ReturnCustomerAddressResponseViewModelTestAsync()
         {
-            var zipCode = CustomerAddressResponseViewModelMock.ZipCodeUnformatedFaker.Generate();
-            var customerService = new CustomerService();
+            var zipCode = AddressResponseViewModelMock.ZipCodeUnformatedFaker.Generate();
+            var viaCepResponseMock = ViaCepAddressResponseModelMock.ViaCepAddressResponseModelFaker.Generate();
+            var addressResponseViewModelMock = AddressResponseViewModelMock.AddressResponseViewModelFaker.Generate();
+
+            _viaCepMock.Setup(x => x.GetAddressAsync(zipCode))
+                .ReturnsAsync(viaCepResponseMock);
+
+            _mapperMock.Setup(x => x.Map<AddressResponseViewModel>(viaCepResponseMock))
+                .Returns(addressResponseViewModelMock);
+
+            var customerService = new AddressService(_viaCepMock.Object, _mapperMock.Object);
             var method = await customerService.GetAddressByZipCodeAsync(zipCode);
-            Assert.IsAssignableFrom<CustomerAddressResponseViewModel>(method);
+            Assert.IsAssignableFrom<AddressResponseViewModel>(method);
         }
     }
 }
